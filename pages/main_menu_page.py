45877d105700977a936eec927d2ec6115aaaf88a
azankov <a.zankov@hypervsn.com>
2021-08-11 19:11:36 +0300

    clic_all_apps method fixed, 'print's  removed

--- pages/main_menu_page.py
+++ pages/main_menu_page.py
@@ -15,12 +15,10 @@ class MainMenuPage(BasePage):
 
     def click_all_apps(self, link):
         apps = self.browser.find_elements(By.CSS_SELECTOR, '[class="o_app o_menuitem"]')
-        print(self.browser.current_url)
         apps_name = []
         for i in apps:
             apps_attr = i.get_attribute("data-menu-xmlid")
             apps_name.append(apps_attr)
-        print(apps_name)
         for i in apps_name:
             self.browser.find_element(By.CSS_SELECTOR, f'[data-menu-xmlid="{i}"]').click()
             WebDriverWait(self.browser, 15).until(
@@ -31,8 +29,9 @@ class MainMenuPage(BasePage):
                 back_to_main = WebDriverWait(self.browser, 15).until(
                     EC.element_to_be_clickable((By.CSS_SELECTOR, '.o_menu_toggle')))
                 back_to_main.click()
-                app_elem = WebDriverWait(self.browser, 9).until(
-                    EC.element_to_be_clickable((By.CSS_SELECTOR, '[class="o_app o_menuitem"]')))
+                app_elem = self.browser.find_element(By.CSS_SELECTOR, '[class="o_app o_menuitem"]')
+                # app_elem = WebDriverWait(self.browser, 9).until(
+                #     EC.element_to_be_clickable((By.CSS_SELECTOR, '[class="o_app o_menuitem"]')))
                 if app_elem:
                     break
 
